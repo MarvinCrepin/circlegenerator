15360a59306aeed55797468382ce028799182f5a
Sayn <marvincrepin@gmail.com>
2022-08-25 15:50:24 +0200

    dev: css & js

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,12 @@
+import { Point } from "./interfaces/Point";
+
 const svgns = "http://www.w3.org/2000/svg";
 const container = document.querySelector("svg g.samples");
-const x0: number = 50;
-const y0: number = 50;
-const r0: number = 45;
-const r: string = String(1);
-const samples: number = 10;
+const x0 = 50;
+const y0 = 50;
+const r0 = 45;
+const r = String(1);
+const samples = 10;
 
 if (!container) {
   throw new Error("Container is null");
@@ -13,23 +15,22 @@ if (!container) {
 const getAngleFromIndex = (index: number, samples = 10): number =>
   (index * 2 * Math.PI) / samples;
 
-const getPositionFromAngle = (angle: number): { x: string; y: string } => {
-  const x: string = String(x0 + r0 * Math.cos(angle));
-  const y: string = String(y0 + r0 * Math.sin(angle));
+const getPositionFromAngle = (angle: number): Point => {
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
   return { x, y };
 };
 
-const createCircleElement = (x: string, y: string) => {
+const createCircleElement = ({ x, y }: Point): SVGCircleElement => {
   const circle = document.createElementNS(svgns, "circle");
-  circle.setAttributeNS(null, "cx", x);
-  circle.setAttributeNS(null, "cy", y);
+  circle.setAttributeNS(null, "cx", String(x));
+  circle.setAttributeNS(null, "cy", String(y));
   circle.setAttributeNS(null, "r", r);
   return circle;
 };
 
 for (let i = 0; i < samples; i++) {
   const angle = getAngleFromIndex(i);
-  const { x, y } = getPositionFromAngle(angle);
-  const circle = createCircleElement(x, y);
+  const circle = createCircleElement(getPositionFromAngle(angle));
   container.appendChild(circle);
 }
